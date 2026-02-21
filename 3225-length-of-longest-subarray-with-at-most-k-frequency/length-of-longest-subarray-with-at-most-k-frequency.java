cdc2cbdc41bf09322a5c25e745c551c8a60dbc42
Umar Firoz <f.umar@op.iitg.ac.in>
2026-02-22 03:49:13 +0530

    Time: 66 ms (78.56%) | Memory: 89.2 MB (41.94%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.java
@@ -4,25 +4,13 @@ class Solution {
         HashMap<Integer, Integer> map= new HashMap<>();
         int max=0;
         for(int r=0;r<nums.length;r++){
-                map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
-                  
-                
+                map.put(nums[r], map.getOrDefault(nums[r], 0) + 1); 
                 while(map.get(nums[r]) > k){
                     map.put(nums[l], map.get(nums[l]) - 1);
-                    if(map.get(nums[l]) ==0)
-                        map.remove(nums[l]);
                     l++;    
                 }
                 max=Math.max(max,r-l+1);
         }
         return max;
-    }
-    public boolean freq(HashMap<Integer, Integer> map,int k) {
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-        if (entry.getValue() > k) {
-                return false;
-            }
-        }
-        return true;
     }
 }
