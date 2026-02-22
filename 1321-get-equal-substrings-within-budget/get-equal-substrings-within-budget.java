9ba675ef4de1e0f8db47592238f394ac5902f200
Umar Firoz <f.umar@op.iitg.ac.in>
2026-02-23 00:26:29 +0530

    Time: 6 ms (91.13%) | Memory: 45 MB (12.51%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int[] arr=new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            arr[i]=Math.abs(s.charAt(i)-t.charAt(i));
+        }
+        int l=0,sum=0,max=0;
+        for(int r=0;r<arr.length;r++){
+            sum+=arr[r];
+            while(sum>maxCost){
+                sum-=arr[l];
+                l++;
+            }
+            max=Math.max(max,r-l+1);
+        }
+        return max;
+    }
+}
