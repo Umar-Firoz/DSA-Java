732011fd68d7f2c8a454c8d923486bb42d51c52d
Umar Firoz <f.umar@op.iitg.ac.in>
2026-02-26 00:56:37 +0530

    Time: 22 ms (96.20%) | Memory: 49.6 MB (5.59%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+         int n=nums.length;
+        int[] ps=new int[n];
+        ps[0]=nums[0];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=1;i<n;i++){
+            ps[i]=ps[i-1]+nums[i];
+        }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(ps[i]==k)
+                c++;
+            int val=ps[i]-k;
+            if(map.containsKey(val)){
+                c+= map.get(val);
+            }
+            map.put(ps[i],map.getOrDefault(ps[i],0)+1);
+        }
+        return c;
+    }
+}
