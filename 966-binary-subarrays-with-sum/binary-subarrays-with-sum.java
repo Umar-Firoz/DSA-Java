d6ca050e3ff5844137a8b2b2d65df14a3d6d91eb
Umar Firoz <f.umar@op.iitg.ac.in>
2026-02-26 18:23:22 +0530

    Time: 18 ms (45.67%) | Memory: 54.2 MB (8.60%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,23 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int k) {
-        int n=nums.length;
-      
-        return binary(nums,k)-binary(nums,k-1);
-    }
-
-    public int binary(int[] nums,int k){
-         if(k < 0) return 0;
-        int l=0;
-        int res=0;
-        int sum=0;
-        for(int r=0;r<nums.length;r++){
-            sum+=nums[r];
-            while(sum>k){
-                sum-=nums[l];
-                l++;
-            }
-            res+=(r-l+1);
+          int n=nums.length;
+        int[] ps=new int[n];
+        ps[0]=nums[0];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=1;i<n;i++){
+            ps[i]=ps[i-1]+nums[i];
         }
-        return res;
-    }
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(ps[i]==k)
+                c++;
+            int val=ps[i]-k;
+            if(map.containsKey(val)){
+                c+= map.get(val);
+            }
+            map.put(ps[i],map.getOrDefault(ps[i],0)+1);
+        }
+        return c;}
 }
