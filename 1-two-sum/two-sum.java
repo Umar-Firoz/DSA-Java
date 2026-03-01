97a56ea1479371eae0e0ab66dd7304ab4b23ac00
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-01 22:59:40 +0530

    Fix two-sum logic to return indices correctly

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -5,7 +5,9 @@ class Solution {
             for(int j=i+1; j<nums.length;j++)
             {
                 if(nums[i]+nums[j]==target)
+                {
                 return new int[] {i, j};
+                }
             }
        }
        return new int[] {};
