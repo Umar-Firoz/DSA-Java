5e57d1ffc8c57782bf42eeef0dc9eef18d251f78
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-04 23:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (66.92%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n=nums.length;
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(nums[m]==target)
+                return m;
+            else if(target>nums[m])
+                s=m+1;
+            else
+                e=m-1;        
+        }
+        return s;
+    }
+}
