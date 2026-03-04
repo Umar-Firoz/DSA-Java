336f23c6c83341236411dda1b87c9dad45acadf8
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-05 00:24:46 +0530

    Time: 14 ms (50.56%) | Memory: 42.1 MB (72.94%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,23 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int s=0;
+        int e=n;
+        int ans=0;
+        while(s<=e){
+            int m=s+(e-s)/2;
+            boolean bad = isBadVersion(m);
+            if(bad)
+            {
+                e=m-1;
+                ans=m;
+            }
+            else{
+                s=m+1;
+            }
+        }
+        return ans;
+    }
+}
