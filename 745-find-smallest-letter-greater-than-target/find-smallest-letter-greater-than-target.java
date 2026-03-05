bd67cad3335d4c1b6819eecddb4fd0f420fe69b4
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-05 19:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (88.92%) - LeetSync

---
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] ar, char target) {
+        int s=0;
+        int e=ar.length;
+        if(target<ar[0])
+            return ar[0];
+        if(target>=ar[e-1])
+            return ar[0];
+        while(s<e){
+            int m=s+(e-s)/2;
+            if(ar[m]<=target)
+                s=m+1;
+            else 
+                e=m;
+        }
+        return ar[s];
+    }
+}
