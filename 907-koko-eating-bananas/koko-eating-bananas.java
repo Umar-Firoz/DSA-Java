1ab895bd35ee16657a6c08a290ae956a3bfe95ac
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-07 16:17:20 +0530

    Time: 19 ms (36.24%) | Memory: 48.2 MB (24.56%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minEatingSpeed(int[] arr, int hours) {
+        int s=1;
+        int e=max(arr);
+        while (s<e){
+            int m=s+(e-s)/2;
+            if(check(arr,m,hours))
+                e=m;
+            else
+                s=m+1;
+        }
+        return s;
+    }
+    static int max(int[] arr){
+        int max=0;
+        for (int i : arr) {
+            max = Math.max(max, i);
+        }
+        return max;
+    }
+       static boolean check(int[] arr,int k,int hours){
+        int h=0;
+        for(int i : arr){
+            if(i<=k)
+                h+=1;
+            else
+                h+= (int) Math.ceil((double)i / k);
+        }
+        return h <= hours;
+    }
+}
