1205f434c62d7f5dad494e233b8b477738aea801
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-11 17:03:27 +0530

    Time: 18 ms (13.96%) | Memory: 46.8 MB (8.08%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        if(s.length()!=t.length())
+            return false;
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(map.containsKey(t.charAt(i))){
+                map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+                if(map.get(t.charAt(i))==0)
+                    map.remove(t.charAt(i));
+            }
+        }
+        if(map.size()==0)
+            return true;
+        return false;    
+
+    }
+}
