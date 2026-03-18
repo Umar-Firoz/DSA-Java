48303658e5c985abe7ba88187a8f291caba2d114
Umar Firoz <f.umar@op.iitg.ac.in>
2026-03-18 16:10:25 +0530

    Time: 9 ms (44.07%) | Memory: 42.1 MB (66.05%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,30 @@
 class Solution {
+
+    //recursion code
     public int fib(int n) {
         if(n<=1)
             return n;
         return fib(n-1)+fib(n-2);    
     }
+
+    // optimized code here
+    
+    public int fibOpt(int n) {
+        int f = 0;
+        int s = 1;
+        int result = 0;
+        if (n == 0) {
+            return f;
+        } else if (n == 1) {
+            return s;
+        }
+        for (int i = 2; i <= n; i++) {
+            int temp = s + f;
+            f = s;
+            s = temp;
+            result = temp;
+        }
+        return result;
+    
+        }
 }
