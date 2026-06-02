4f3df52bab4da7e9820ac79c973d49e38734d410
Umar Firoz <f.umar@op.iitg.ac.in>
2026-06-03 00:05:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (98.88%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int sum =0;
+        int max=gain[0];
+        for(int i=1;i<gain.length;i++)
+        {
+            gain[i]=gain[i]+gain[i-1];
+            if(gain[i]>max){
+                max=gain[i];
+            }
+        }
+        if(max<0)
+        {
+            return 0;
+        }
+        return max;
+    }
+}
