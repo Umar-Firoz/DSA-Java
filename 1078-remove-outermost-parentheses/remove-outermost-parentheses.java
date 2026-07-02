1a9a1fbb8ccc8a5b4b532654619c86cf93511b25
Umar Firoz <f.umar@op.iitg.ac.in>
2026-07-02 23:59:45 +0530

    Time: 2 ms (99.74%) | Memory: 43.6 MB (41.03%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,19 +1,42 @@
 class Solution {
     public String removeOuterParentheses(String s) {
    StringBuilder ans = new StringBuilder();
+   int c=0;
         Stack<Character> stack = new Stack<>();
         for (char ch : s.toCharArray()) {
             if (ch == '(') {
-                if (!stack.isEmpty()) {
+                if (c>0) {
                     ans.append(ch);
                 }
-                stack.push(ch);
+                c++;
             } else {
-                stack.pop();
-                if (!stack.isEmpty()) {
+                c--;
+                if (c>0) {
                     ans.append(ch);
-                }}
+                }
+                }
         }
         return ans.toString();
     }
 }
+
+//stack solutin this is not optimal
+// class Solution {
+//     public String removeOuterParentheses(String s) {
+//    StringBuilder ans = new StringBuilder();
+//         Stack<Character> stack = new Stack<>();
+//         for (char ch : s.toCharArray()) {
+//             if (ch == '(') {
+//                 if (!stack.isEmpty()) {
+//                     ans.append(ch);
+//                 }
+//                 stack.push(ch);
+//             } else {
+//                 stack.pop();
+//                 if (!stack.isEmpty()) {
+//                     ans.append(ch);
+//                 }}
+//         }
+//         return ans.toString();
+//     }
+// }
